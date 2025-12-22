c32de88bed79429d0acb0db9917ae0822d0d0e9c
Anton <a.kronosjo@gmail.com>
2025-12-22 01:04:43 +0100

    Some changes

--- VersionedContentPOC.Server/Attributes/ContentPropertyMetaDataAttribute.cs
+++ VersionedContentPOC.Server/Attributes/ContentPropertyMetaDataAttribute.cs
@@ -1,19 +1,23 @@
-﻿namespace VersionedContentPOC.Attributes;
+﻿using VersionedContentPOC.Server.Attributes;
+
+namespace VersionedContentPOC.Attributes;
 
 [AttributeUsage(AttributeTargets.Property)]
 public class ContentPropertyMetadataAttribute : Attribute
 {
     public bool Editable { get; private set; }
+
     public bool Required { get; private set; }
+
+    [ShouldBeRefactored("This property is not used")]
     public Editor PropertyEditor { get; private set; }
 
-    public ContentPropertyMetadataAttribute(bool editable = false, bool required = false, Editor propertyEditor = Editor.Input)
+    public ContentPropertyMetadataAttribute(bool editable = false, bool required = false, Editor editor = Editor.Input)
     {
         Editable = editable;
         Required = required;
     }
 }
-
 
 public enum Editor {
     Input,
