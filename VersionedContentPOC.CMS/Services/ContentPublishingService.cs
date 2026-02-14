58215d2e0ed1aef654533a81025361976e4f0f3b
Anton <a.kronosjo@gmail.com>
2026-02-14 13:02:19 +0100

    Some refactoring for ContentPublishingService

--- VersionedContentPOC.CMS/Services/ContentPublishingService.cs
+++ VersionedContentPOC.CMS/Services/ContentPublishingService.cs
@@ -1,17 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
-using VersionedContentPOC.CMS.Attributes;
+﻿using VersionedContentPOC.CMS.Attributes;
 using VersionedContentPOC.CMS.Data;
 using VersionedContentPOC.CMS.Data.Enums;
 using VersionedContentPOC.CMS.Data.Models;
-using VersionedContentPOC.CMS.Extensions;
 
 namespace VersionedContentPOC.CMS.Services;
 
 public interface IContentPublishingService
 {
     void Publish(int versionId);
-    void Unpublish(int versionId);
     void Publish(Content content);
+    void Unpublish(int versionId);
     void Unpublish(Content content);
 }
 internal class ContentPublishingService : IContentPublishingService
@@ -34,8 +32,11 @@ internal class ContentPublishingService : IContentPublishingService
         if (content.Status == PublishStatus.Published)
             throw new Exception("Content already published!");
 
-        using var transaction = _context.Database.BeginTransaction();
-        var currentlyPublishedVersion = _context.Content.FirstOrDefault(x => x.ContentId == content.ContentId && x.Status == PublishStatus.Published);
+        var currentlyPublishedVersion = _context.Content.FirstOrDefault(x =>
+            x.ContentId == content.ContentId
+            && x.Status == PublishStatus.Published
+        );
+
         if (currentlyPublishedVersion != null)
         {
             currentlyPublishedVersion.Status = PublishStatus.Unpublished;
@@ -47,25 +48,15 @@ internal class ContentPublishingService : IContentPublishingService
         content.StopPublish = null;
         content.Status = PublishStatus.Published;
         _context.Update(content);
-        UpdateMainLanguageOnlyValues(content);
+        
         _context.SaveChanges();
-        transaction.Commit();
     }
 
     [ShouldBeRefactored("This does not take in consideration if content has been unpublished on main language or similar")]
     public void Unpublish(Content content)
     {
-        using var transaction = _context.Database.BeginTransaction();
         content.StopPublish = DateTime.UtcNow;
         content.Status = PublishStatus.Unpublished;
-        _context.SaveChanges();
-
-        var root =_context.ContentRoots.Include(x => x.Versions).Single(x => x.ContentId == content.ContentId);
-        var publishedOrLastCreatedVersion = root.Versions.GetCurrentlyPublishedOrLastCreated();
-        if (publishedOrLastCreatedVersion == null)
-            throw new Exception("No published or last created version exist!");
-
-        UpdateMainLanguageOnlyValues(publishedOrLastCreatedVersion);
         _context.SaveChanges();
     }
 
@@ -73,39 +64,5 @@ internal class ContentPublishingService : IContentPublishingService
     {
         var content = _context.Content.Single(x => x.VersionId == versionId);
         Unpublish(content);
-    }
-
-    public void DelayedPublish(Content contentDraft)
-    {
-        throw new NotImplementedException();
-    }
-
-    private void UpdateMainLanguageOnlyValues(Content mainLanguageVersion)
-    {
-        var root = _context.ContentRoots
-            .Include(x => x.Versions)
-            .Single(x => x.ContentId == mainLanguageVersion.ContentId);
-
-        if (root.MainLanguage == mainLanguageVersion.Language)
-        {
-            foreach (var version in root.Versions.Where(x => x.Language != root.MainLanguage))
-            {
-                CopyMainLanguageValues(version, mainLanguageVersion);
-                _context.Update(version);
-            }
-        }
-    }
-
-    private static void CopyMainLanguageValues<T>(T target, T source) where T : Content
-    {
-        if (target.GetType() != source.GetType()) throw new ArgumentException("Types differ!");
-
-        var properties = source
-            .GetType()
-            .GetContentProperties()
-            .FilterByAttribute<MainLanguageOnlyAttribute>(x => x?.IsActive == true);
-
-        foreach (var p in properties)
-            p.SetValue(target, p.GetValue(source));
     }
 }
